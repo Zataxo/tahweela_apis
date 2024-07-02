9ebdbb89d2f442c9af0e20a6ddeb22494116b110
Hassan Ismat <hassan.og@outlook.com>
2024-07-02 23:35:46 +0300

    adding user's transactions

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -15,5 +15,18 @@ class TransactionsController {
       res.status(400).json(vR);
     }
   };
+  static getUserTransactions = (req, res) => {
+    if (!req.params.userId) {
+      return res.status(400).send("No User identified");
+    }
+    models.Transactions.findAll({ where: { userId: req.params.userId } }).then(
+      (usersTransactions) => {
+        if (usersTransactions.length === 0) {
+          return res.status(404).json(usersTransactions);
+        }
+        res.status(200).json(usersTransactions);
+      }
+    );
+  };
 }
 module.exports = TransactionsController;
--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -2,4 +2,8 @@ const express = require("express");
 const transactionsRouter = express.Router();
 const transactionController = require("../controllers/transactions.controller");
 transactionsRouter.post("/create", transactionController.createTransaction);
+transactionsRouter.get(
+  "/getUserTransactions/:userId",
+  transactionController.getUserTransactions
+);
 module.exports = transactionsRouter;
